093dfd7691eaabb67b2b7a97823cc99c901e537c
Zenobia LAM <zenobia.8.lam@gmail.com>
2024-12-29 18:07:29 +0800

    Math.max

--- README.js
+++ README.js
@@ -7,3 +7,14 @@ let base = 20
 let topping = 5
 let sum = base+topping 
 console.log(sum)
+//
+let name = "Zeno"
+console.log(name+"!")
+//
+let price = 10
+price = 12
+console.log(price)
+//
+let sum1 = (1+2)
+let result = Math.max(2,sum1)
+console.log(result)
